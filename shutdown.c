78ff8a9f7ee4f06eb6a09a0634770cafbcca6545
justin williams <notoxy.xxo@icloud.com>
2026-02-13 16:28:37 +0000

    Finished Project 2

---
+++ shutdown.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+/* Remove the following code when heading to next task */
+/********* Start ********/
+
+/******* end *******/
+
+int main(int arc, char *argv[]) 
+{
+    printf(1,"\nBYEBYE XV6\n\n");
+    shutdown();
+    exit();
+}
